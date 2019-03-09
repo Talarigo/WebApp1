f83d744ee0060e26bcb3d2500784781efe9be352
rajbox <raj.j.box@gmail.com>
2019-03-08 22:45:32 -0500

    Expanded RulesGrid in JSON string

--- WebApp1/wwwroot/js/site.js
+++ WebApp1/wwwroot/js/site.js
@@ -10,11 +10,39 @@ const triggerWord = 'hello';
 var strFunctionJSON =
 '{ "CommandFunctions" : '+
     '['+
-        '{ "context": "0", "command": "start", "function": "onStartRecipe()" },'+
-        '{ "context": "0", "command": "stop", "function": "onStopRecipe()" },'+
-        '{ "context": "0", "command": "pause", "function": "onPauseRecipe()" },'+
-        '{ "context": "", "command": "unknown", "function": "onUnknown()" }'+
-    ']'+
+        '{ "context":"0", "command":"start", "function":"onStartRecipe()"},' +
+        '{ "context":"0", "command":"name", "function":"onAnnounceTitle()"},' +
+        '{ "context":"0", "command":"title", "function":"onAnnounceTitle()"},' +
+        '{ "context":"0", "command":"description", "function":"onAnnounceDescription()"},' +
+        '{ "context":"0", "command":"ingredients", "function":"onListIngredients()"},' +
+        '{ "context":"0", "command":"tools", "function":"onListToolsNeeded()"},' +
+        '{ "context":"0", "command":"list steps", "function":"onListSteps()"},' +
+        '{ "context":"0", "command":"add comment", "function":"onAddComment()"},' +
+        '{ "context":"0", "command":"read comments", "function":"onReadComments()"},' +
+
+        '{ "context":"1", "command":"start", "function":"onStartStep()"},' +
+        '{ "context":"1", "command":"name", "function":"onAnnounceTitle()"},' +
+        '{ "context":"1", "command":"title", "function":"onAnnounceTitle()"},' +
+        '{ "context":"1", "command":"description", "function":"onAnnounceDescription()"},' +
+        '{ "context":"1", "command":"pause", "function":"onPauseStep()"},' +
+        '{ "context":"1", "command":"continue", "function":"onContinueStep()"},' +
+        '{ "context":"1", "command":"stop", "function":"onStopStep()"},' +
+        '{ "context":"1", "command":"skip step", "function":"onSkipStep()"},' +
+        '{ "context":"1", "command":"ingredients", "function":"onListIngredients()"},' +
+        '{ "context":"1", "command":"tools", "function":"onListTools()"},' +
+        '{ "context":"1", "command":"add comment", "function":"onAddComment()"},' +
+        '{ "context":"1", "command":"read comments", "function":"onReadComments()"},' +
+        '{ "context":"1", "command":"restart step", "function":"onStartStep()"},' +
+
+        '{ "context":"", "command":"substitute", "function":"onIngredientSubstitute()"},' +
+        '{ "context":"", "command":"convert", "function":"onUnitConverstion()"},' +
+        '{ "context":"", "command":"start timer", "function":"onStartTimer()"},' +
+        '{ "context":"", "command":"cancel timer", "function":"onCancelTimer()"},' +
+        '{ "context":"", "command":"end timer", "function":"onEndTimer()"},' +
+        '{ "context":"", "command":"list timers", "function":"onListTimers()"},' +
+        '{ "context":"", "command":"unknown", "function":"onUnknown()"},' +
+        '{ "context":"", "command":"end", "function":"onExitRecipe()"}' +
+    ']' +
 '}';
 //------------------------------------------------------------
 //.....  Array of functions : Method 2
@@ -28,7 +56,7 @@ for (i = 0; i < JSONObj.CommandFunctions.length; i++) {
 
 //------------------------------------------------------------
 
-function onStartRecipe()
+function onStartStep()
 {
     var msg = new SpeechSynthesisUtterance("Let's get cooking!");
     window.speechSynthesis.speak(msg);
@@ -38,7 +66,7 @@ function onStartRecipe()
     //.. Launch step 1
 }
 
-function onStopRecipe()
+function onStopStep()
 {
     //.. Note where you were.  Just in case user decides to continue...
 
@@ -52,7 +80,7 @@ function onStopRecipe()
     //.. Now, should we redirect to home page??  Or just stay on this page
 }
 
-function onPauseRecipe()
+function onPauseStep()
 {
     var msg = new SpeechSynthesisUtterance("Paused!Press Start when ready!");
     window.speechSynthesis.speak(msg);
@@ -74,15 +102,15 @@ function doCommand(strContext, strSpeech)
     //..         NOTE: strSpeech is ALWAYS lowercase
     if (strSpeech.includes("start"))
     {
-        strCommand = strContext+'start';
+        strCommand = strContext + 'start';
     }
     else if (strSpeech.includes("stop"))
     {
-        strCommand = strContext+'stop';
+        strCommand = strContext + 'stop';
     }
     else if (strSpeech.includes("pause"))
     {
-        strCommand = strContext+'pause';
+        strCommand = strContext + 'pause';
     }
     else
     {
@@ -105,7 +133,7 @@ function parseSpeech(speech) {
         $('#textFromSpeech').val("Parsing Failed!");
     }
 
-    doCommand('0', arr[1]); //.. first argument is the context
+    doCommand('1', arr[1]); //.. first argument is the context
 }
 
 function say(m) {
